f98ed778b4800c7e899cc6ce8b5a153d0b623434
Ivan Zhitnikov <zhitnikov.ivan98@gmail.com>
2020-02-20 18:08:43 +0300

    Update0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vztest-project-pkg-ivanz98",
-    version="0.0.5",
+    version="0.0.6",
     author="ivanz98",
     author_email="zhitnikov.ivan98@gmail.com",
     description="A test example package",
@@ -17,5 +17,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
