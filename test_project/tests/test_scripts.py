cfab8b772f95d112f11499f0e72176aff70ce621
Ivan Zhitnikov <zhitnikov.ivan98@gmail.com>
2020-02-26 15:34:38 +0300

    add another sum test

--- test_project/tests/test_scripts.py
+++ test_project/tests/test_scripts.py
@@ -7,6 +7,7 @@ class TestScripts(unittest.TestCase):
     def test_add(self):
         self.assertEqual(15, add(10, 5))
         self.assertEqual(11, add(10, 1))
+        self.assertEqual(500, add(250, 250))
 
     def test_sub(self):
         self.assertEqual(5, sub(10, 5))
